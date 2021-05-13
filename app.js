5f91102288e59175b87705d8560dd7a4b06b5906
unknown <tomuleseidimitrie@gmail.com>
2021-05-13 15:09:36 +0300

    ejs

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+
+const orice = "asdasd";
 app.get("/", (req, res) => {
-  res.send("asdasdasdasdasd");
+  res.render("home", { orice });
 });
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  res.render("show", { id });
+});
+
 app.listen(3000, () => console.log("connection open on port 3000"));
