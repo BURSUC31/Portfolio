7fe6282a1b941486108f8291146ded34fbfbe5f0
unknown <tomuleseidimitrie@gmail.com>
2021-05-17 14:53:24 +0300

    mvc

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const path = require("path");
+const ejsMate = require("ejs-mate");
 const app = express();
 app.set("view engine", "ejs");
-
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 const orice = "asdasd";
 app.get("/", (req, res) => {
   res.render("home", { orice });
